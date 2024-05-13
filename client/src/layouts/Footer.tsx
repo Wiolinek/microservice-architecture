05765d208105a4205e65c92a7844054998254dce
Wiola P <wiol@onet.eu>
2024-05-13 22:16:16 +0200

    remove fontawesome, add Material UI icons

--- client/src/layouts/Footer.tsx
+++ client/src/layouts/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
   const year: number = new Date().getFullYear();
 
   return (
-    <Toolbar sx={{ position: 'absolute', bottom: 10, left: 0, right: 0, bgcolor: 'primary.main', justifyContent: 'center'}} role="footer">
-      <Typography variant="body1" component="p" sx={{  color: '#fff' }}>
+    <Toolbar sx={{ position: 'absolute', bottom: 0, left: 0, right: 0, bgcolor: 'primary.main', justifyContent: 'center' }} role="footer">
+      <Typography variant="body1" component="p" sx={{ color: '#fff' }}>
         {`Copyright \u00A9 ${year} Wiola Polok www.u-v.codes. All Rights Reserved`}
       </Typography>
     </Toolbar>
