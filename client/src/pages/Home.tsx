58dea7c3cf34095ed040cef714d6d2d646f59d2d
Wiola P <wiol@onet.eu>
2024-05-16 20:56:35 +0200

    home page style update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import PageWrapper from '../components/PageWrapper';
 import RidesList from '../components/RidesList';
 
 const Home = () => {
   return (
     <PageWrapper>
-      <Stack spacing={2}>
+      <Stack direction="column" alignItems="center" spacing={6}>
+        <Box display="flex" flexDirection='column' gap={2}>
+          <Typography variant="h4" component="h1" sx={{ fontSize: '4rem', fontWeight: 900 }}>
+            Find your perfect ride
+          </Typography>
+          <Typography variant="h4" component="h2" sx={{ fontWeight: 700, color: 'primary.main' }}>
+            for the best price
+          </Typography>
+        </Box>
         <RidesList />
       </Stack>
     </PageWrapper>
