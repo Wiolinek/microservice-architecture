1962ad9a16258d2c754670d1a62d9059910fcf08
Wiola P <wiol@onet.eu>
2024-05-16 21:07:50 +0200

    ride card style

--- client/src/components/RideCard.tsx
+++ client/src/components/RideCard.tsx
@@ -5,35 +5,104 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import EastIcon from '@mui/icons-material/East';
+import PersonIcon from '@mui/icons-material/Person';
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PaidIcon from '@mui/icons-material/Paid';
 
-const RideCard = () => {
+interface RideCardProps {
+  start: string;
+  destination: string;
+  startTime: string;
+  endTime: string;
+  totalSeats: number;
+  freeSeats: number;
+  price: number;
+  carMake: string;
+  photo: string;
+}
+
+const typographyStyle = {
+  fontWeight: 700,
+  letterSpacing: '.1rem',
+  textTransform: 'uppercase',
+};
+
+const generateSeats = (totalSeats: number, freeSeats: number) => {
+  const bookedSeats = totalSeats - freeSeats;
+  const icons = [];
+
+  for (let i = 0; i < freeSeats; i++) {
+    icons.push(<PersonIcon sx={{ color: 'green' }} />);
+  }
+  for (let i = 0; i < bookedSeats; i++) {
+    icons.push(<PersonIcon sx={{ color: 'grey' }} />);
+  }
+  return icons;
+};
+
+const RideCard = ({ start, destination, totalSeats, startTime, endTime, freeSeats, price, carMake, photo }: RideCardProps) => {
   return (
-    <Grid item xs={12} sm={6} md={4}>
+    <Grid item xs={12} md={6} xl={4}>
       <Paper sx={{ p: 2 }}>
-        <Stack>
-          <Typography variant="body1" component="p">
-            title
-          </Typography>
-          <ImageList variant="masonry" cols={3} gap={8}>
-            <ImageListItem>
-              <img
-                // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                // src={`${item.img}?w=248&fit=crop&auto=format`}
-                alt={'car image'}
-                loading="lazy"
-              />
+        <Stack direction="column" alignItems="center" spacing={2}>
+          <Stack direction="row" alignItems="center" spacing={2}>
+            <MyLocationIcon />
+            <Stack alignItems="center" spacing={2}>
+              <Typography variant="h6" component="p" sx={typographyStyle}>
+                {start}
+              </Typography>
+              <Typography
+                variant="body2"
+                component="p"
+                sx={{
+                  fontWeight: 700,
+                  letterSpacing: '.1rem',
+                  textTransform: 'uppercase',
+                }}>
+                {startTime}
+              </Typography>
+            </Stack>
+            <EastIcon />
+            <Stack alignItems="center" spacing={2}>
+              <Typography variant="h6" component="p" sx={typographyStyle}>
+                {destination}
+              </Typography>
+              <Typography variant="body2" component="p" sx={typographyStyle}>
+                {endTime}
+              </Typography>
+            </Stack>
+            <LocationOnIcon />
+          </Stack>
+          <Divider flexItem orientation="horizontal" sx={{ backgroundColor: 'primary.main', height: 1 }} />
+          <ImageList variant="quilted" cols={1} sx={{ width: '100%', height: '300px' }}>
+            <ImageListItem
+              sx={{ overflow: 'hidden', display: 'flex', alignItems: 'center' }}>
+              <img src={photo} alt={carMake} title={carMake} loading="lazy" />
             </ImageListItem>
           </ImageList>
-          <Stack direction="row" spacing={1}>
-            <Typography variant="body1" component="p">
-              total seats
-            </Typography>
-            <Typography variant="body1" component="p">
-              free seats
-            </Typography>
-            <Typography variant="body1" component="p">
-              title
-            </Typography>
+          <Stack direction="row" justifyContent="space-between" spacing={1} sx={{ width: '100%' }}>
+            <Box sx={{ display: 'flex', direction: 'row', alignItems: 'center', gap: 1 }}>
+              <Typography
+                variant="body1"
+                component="p"
+                sx={{
+                  ...typographyStyle,
+                  display: { xs: 'none', sm: 'flex' },
+                }}>
+                Available seats:
+              </Typography>
+              {generateSeats(totalSeats, freeSeats)}
+            </Box>
+            <Box sx={{ display: 'flex', direction: 'row', alignItems: 'center', gap: 1 }}>
+              <PaidIcon />
+              <Typography variant="h6" component="p" sx={typographyStyle}>
+                {price} PLN
+              </Typography>
+            </Box>
           </Stack>
           <Button variant="contained" color="primary" size="medium" sx={{ textDecoration: 'none', width: '100%' }}>
             Book seat
--- client/src/components/RidesList.tsx
+++ client/src/components/RidesList.tsx
@@ -1,23 +1,86 @@
-import Stack from '@mui/material/Stack';
 import RideCard from '../components/RideCard';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+
+const rides = [
+  {
+    id: 1,
+    start: 'Warszawa',
+    destination: 'Kraków',
+    startTime: '09:00',
+    endTime: '12:30',
+    totalSeats: 3,
+    freeSeats: 1,
+    price: 10,
+    carMake: 'Skoda',
+    photo: '../../images/1.jpg',
+  },
+  {
+    id: 2,
+    start: 'Sopot',
+    destination: 'Gdańsk',
+    startTime: '14:00',
+    endTime: '14:30',
+    totalSeats: 2,
+    freeSeats: 2,
+    price: 10,
+    carMake: 'Opel',
+    photo: '../../images/2.jpg',
+  },
+  {
+    id: 3,
+    start: 'Poznań',
+    destination: 'Wrocław',
+    startTime: '10:00',
+    endTime: '12:15',
+    totalSeats: 2,
+    freeSeats: 1,
+    price: 10,
+    carMake: 'BMW',
+    photo: '../../images/3.jpg',
+  },
+  {
+    id: 4,
+    start: 'Łódź',
+    destination: 'Warszawa',
+    startTime: '18:00',
+    endTime: '19:45',
+    totalSeats: 3,
+    freeSeats: 2,
+    price: 10,
+    carMake: 'Mercedes',
+    photo: '../../images/4.jpg',
+  },
+  {
+    id: 5,
+    start: 'Katowice',
+    destination: 'Opole',
+    startTime: '06:00',
+    endTime: '07:20',
+    totalSeats: 2,
+    freeSeats: 1,
+    price: 10,
+    carMake: 'BMW',
+    photo: '../../images/5.jpg',
+  },
+];
 
 const RidesList = () => {
   return (
-    <Stack direction="column" alignItems="center" spacing={3}>
-      <Typography variant="h4" component="h1">
-        title
-      </Typography>
       <Grid container spacing={3} style={{ marginLeft: -24 }}>
-        <RideCard></RideCard>
-        <RideCard></RideCard>
-        <RideCard></RideCard>
-        <RideCard></RideCard>
-        <RideCard></RideCard>
-        <RideCard></RideCard>
+        {rides.map((ride) => (
+          <RideCard
+            key={ride.id}
+            start={ride.start}
+            destination={ride.destination}
+            startTime={ride.startTime}
+            endTime={ride.endTime}
+            totalSeats={ride.totalSeats}
+            freeSeats={ride.freeSeats}
+            price={ride.price}
+            carMake={ride.carMake}
+            photo={ride.photo}></RideCard>
+        ))}
       </Grid>
-    </Stack>
   );
 };
 
