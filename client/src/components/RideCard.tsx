176f22423d8f05196120a148fec0a8479494a4d5
Wiola P <wiol@onet.eu>
2024-05-17 12:54:10 +0200

    add ListItem MUI

--- client/src/components/RideCard.tsx
+++ client/src/components/RideCard.tsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
 import EastIcon from '@mui/icons-material/East';
 import PersonIcon from '@mui/icons-material/Person';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
@@ -47,68 +48,69 @@ const generateSeats = (totalSeats: number, freeSeats: number) => {
 const RideCard = ({ start, destination, totalSeats, startTime, endTime, freeSeats, price, carMake, photo }: RideCardProps) => {
   return (
     <Grid item xs={12} md={6} xl={4}>
-      <Paper sx={{ p: 2 }}>
-        <Stack direction="column" alignItems="center" spacing={2}>
-          <Stack direction="row" alignItems="center" spacing={2}>
-            <MyLocationIcon />
-            <Stack alignItems="center" spacing={2}>
-              <Typography variant="h6" component="p" sx={typographyStyle}>
-                {start}
-              </Typography>
-              <Typography
-                variant="body2"
-                component="p"
-                sx={{
-                  fontWeight: 700,
-                  letterSpacing: '.1rem',
-                  textTransform: 'uppercase',
-                }}>
-                {startTime}
-              </Typography>
+      <ListItem disableGutters sx={{ display: 'block'}}>
+        <Paper sx={{ p: 2 }}>
+          <Stack direction="column" alignItems="center" spacing={2}>
+            <Stack direction="row" alignItems="center" spacing={2}>
+              <MyLocationIcon />
+              <Stack alignItems="center" spacing={2}>
+                <Typography variant="h6" component="p" sx={typographyStyle}>
+                  {start}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  sx={{
+                    fontWeight: 700,
+                    letterSpacing: '.1rem',
+                    textTransform: 'uppercase',
+                  }}>
+                  {startTime}
+                </Typography>
+              </Stack>
+              <EastIcon />
+              <Stack alignItems="center" spacing={2}>
+                <Typography variant="h6" component="p" sx={typographyStyle}>
+                  {destination}
+                </Typography>
+                <Typography variant="body2" component="p" sx={typographyStyle}>
+                  {endTime}
+                </Typography>
+              </Stack>
+              <LocationOnIcon />
             </Stack>
-            <EastIcon />
-            <Stack alignItems="center" spacing={2}>
-              <Typography variant="h6" component="p" sx={typographyStyle}>
-                {destination}
-              </Typography>
-              <Typography variant="body2" component="p" sx={typographyStyle}>
-                {endTime}
-              </Typography>
+            <Divider flexItem orientation="horizontal" sx={{ backgroundColor: 'primary.main', height: 1 }} />
+            <ImageList variant="quilted" cols={1} sx={{ width: '100%', height: '300px' }}>
+              <ImageListItem sx={{ overflow: 'hidden', display: 'flex', alignItems: 'center' }}>
+                <img src={photo} alt={carMake} title={carMake} loading="lazy" />
+              </ImageListItem>
+            </ImageList>
+            <Stack direction="row" justifyContent="space-between" spacing={1} sx={{ width: '100%' }}>
+              <Box sx={{ display: 'flex', direction: 'row', alignItems: 'center', gap: 1 }}>
+                <Typography
+                  variant="body1"
+                  component="p"
+                  sx={{
+                    ...typographyStyle,
+                    display: { xs: 'none', sm: 'flex' },
+                  }}>
+                  Available seats:
+                </Typography>
+                {generateSeats(totalSeats, freeSeats)}
+              </Box>
+              <Box sx={{ display: 'flex', direction: 'row', alignItems: 'center', gap: 1 }}>
+                <PaidIcon />
+                <Typography variant="h6" component="p" sx={typographyStyle}>
+                  {price} PLN
+                </Typography>
+              </Box>
             </Stack>
-            <LocationOnIcon />
+            <Button variant="contained" color="primary" size="medium" sx={{ textDecoration: 'none', width: '100%' }}>
+              Book seat
+            </Button>
           </Stack>
-          <Divider flexItem orientation="horizontal" sx={{ backgroundColor: 'primary.main', height: 1 }} />
-          <ImageList variant="quilted" cols={1} sx={{ width: '100%', height: '300px' }}>
-            <ImageListItem
-              sx={{ overflow: 'hidden', display: 'flex', alignItems: 'center' }}>
-              <img src={photo} alt={carMake} title={carMake} loading="lazy" />
-            </ImageListItem>
-          </ImageList>
-          <Stack direction="row" justifyContent="space-between" spacing={1} sx={{ width: '100%' }}>
-            <Box sx={{ display: 'flex', direction: 'row', alignItems: 'center', gap: 1 }}>
-              <Typography
-                variant="body1"
-                component="p"
-                sx={{
-                  ...typographyStyle,
-                  display: { xs: 'none', sm: 'flex' },
-                }}>
-                Available seats:
-              </Typography>
-              {generateSeats(totalSeats, freeSeats)}
-            </Box>
-            <Box sx={{ display: 'flex', direction: 'row', alignItems: 'center', gap: 1 }}>
-              <PaidIcon />
-              <Typography variant="h6" component="p" sx={typographyStyle}>
-                {price} PLN
-              </Typography>
-            </Box>
-          </Stack>
-          <Button variant="contained" color="primary" size="medium" sx={{ textDecoration: 'none', width: '100%' }}>
-            Book seat
-          </Button>
-        </Stack>
-      </Paper>
+        </Paper>
+      </ListItem>
     </Grid>
   );
 };
