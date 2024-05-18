8eb583e0e30a43a89ce444a97bcccdf4e51cab28
Wiola P <wiol@onet.eu>
2024-05-18 16:43:51 +0200

    add error page

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import PageWrapper from '@layouts/PageWrapper';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const ErrorPage = () => {
+  return (
+    <PageWrapper>
+      <Stack direction="column" alignItems="center" spacing={6}>
+        <Box display="flex" flexDirection="column" gap={2}>
+          <Typography variant="h4" component="h1" sx={{ fontSize: '4rem', fontWeight: 900 }}>
+            Ooops.. 
+          </Typography>
+          <Typography variant="h4" component="h2" sx={{ fontWeight: 700, color: 'primary.main' }}>
+            Back to home page <Link to='/'>Back</Link>
+          </Typography>
+        </Box>
+      </Stack>
+    </PageWrapper>
+  );
+};
+
+export default ErrorPage;
