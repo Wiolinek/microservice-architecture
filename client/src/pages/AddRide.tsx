0ee6b631b011a7a43a854a53943fe612a141c22d
Wiola P <wiol@onet.eu>
2024-05-19 23:20:44 +0200

    AddRide page banner when user not logged in

--- client/src/pages/AddRide.tsx
+++ client/src/pages/AddRide.tsx
@@ -1,10 +1,22 @@
+import { Link } from 'react-router-dom';
 import PageWrapper from '@layouts/PageWrapper';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Banner from '@layouts/Banner';
 
 const AddRide = () => {
   return (
     <PageWrapper>
-      <Stack spacing={2}>Add Ride</Stack>
+      <Stack direction="column" alignItems="center" spacing={6}>
+        <Banner subtitle="You need to be logged in to add a ride..." />
+      </Stack>
+      <Stack direction="row" alignItems="center" justifyContent="flex-start" spacing={6} style={{ marginTop: 48 }}>
+        <Link to="/login" style={{ alignSelf: 'self-start' }}>
+          <Button variant={'contained'} size="medium" sx={{ color: 'primary.light', backgroundColor: 'primary.main' }}>
+            Please, log in here
+          </Button>
+        </Link>
+      </Stack>
     </PageWrapper>
   );
 };
