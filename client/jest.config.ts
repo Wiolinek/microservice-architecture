658f831a718d30c3df5e2f446cb9a6cc9a22ab79
Wiola P <wiol@onet.eu>
2024-05-22 11:22:01 +0200

    test coverage setup

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -14,4 +14,20 @@ export default {
     "@layouts(.*)$": "<rootDir>/layouts$1",
     "@data(.*)$": "<rootDir>/data$1",
   },
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "<rootDir>/**/*.tsx",
+    "<rootDir>/layouts/**/*.tsx",
+    "<rootDir>/components/**/*.tsx",
+    "<rootDir>/features/**/*.tsx",
+    "<rootDir>/pages/**/*.tsx",
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
