e599a60a9dec8304f5de2d0fd05e9e5fa1ecff40
Wiola P <wiol@onet.eu>
2024-06-04 17:52:18 +0200

    api gateway service, add winston logger to server

--- api-gateway-service/src/server.ts
+++ api-gateway-service/src/server.ts
@@ -1,34 +1,21 @@
 require('dotenv').config();
 import express from 'express';
 // import { StatusCodes } from 'http-status-codes';
-import cookieParser from 'cookie-parser';
-// import { Logger } from 'winston';
-import winston from 'express-winston';
-import { format, transports } from 'winston';
+import { Logger } from 'winston';
+import { winstonLogger } from '@api-gateway-service/logger';
 import cookieSession from 'cookie-session';
 import compression from 'compression';
 import cors from 'cors';
 import hpp from 'hpp';
 import helmet from 'helmet';
 import config from './config';
+// import { ErrorResponse } from '@api-gateway-service/interfaces/api-gateway';
 // import checkConnection from './elasticsearch';
 
 const app = express();
 
-// const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
 
-app.use(
-  winston.logger({
-    transports: [
-      //where I want to save logs
-      new transports.Console(),
-      new transports.File({ level: 'warn', filename: './logs/warnLogs.log' }),
-      new transports.File({ level: 'error', filename: './logs/errorLogs.log' }),
-    ],
-    format: format.combine(format.json(), format.timestamp(), format.colorize(), format.simple(), format.prettyPrint()),
-    statusLevels: true,
-  })
-);
 
 app.set('trust proxy', 1);
 app.use(
@@ -58,19 +45,25 @@ app.use(
 app.use(compression());
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ extended: false, limit: '200mb' }));
-app.use(cookieParser());
 
 //routy
 
 // elastic
 // checkConnection();
 
-
 // error handler
-// app.use('*', (_req: express.Request, res: express.Response, next: express.NextFunction) => {
-//   // const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-//   // log.log('error', fullUrl + 'does not exist');
+// app.use('*', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+//   log.log('error', fullUrl + 'does not exist');
 //   res.status(StatusCodes.NOT_FOUND).json({ message: 'Not found' });
+//   next();
+// });
+
+// app.use((error: ErrorResponse, _req: express.Request, res: express.Response, next: express.NextFunction) => {
+//   log.log('error', `Api Gateway Service ${error.comingFrom}:`, error);
+//   if (error) {
+//     res.status(error.statusCode).json(error.serializeErrors());
+//   }
 //   next();
 // });
 
