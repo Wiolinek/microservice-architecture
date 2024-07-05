ea0c161141b8548b242fe5029652258abb15d008
Wiola P <wiol@onet.eu>
2024-07-05 17:26:28 +0200

    Rabbit connection method update

--- authentication-service/src/queues/connection.ts
+++ authentication-service/src/queues/connection.ts
@@ -9,11 +9,11 @@ async function createConnection(): Promise<Channel | undefined> {
   try {
     const connection: Connection = await client.connect(`${config.RABBITMQ_ENDPOINT}`);
     const channel: Channel = await connection.createChannel();
-    log.info('Notification server connected to queue successfully...');
+    log.info('Authentication server connected to queue successfully...');
     closeConnection(channel, connection);
     return channel;
   } catch (error) {
-    log.log('error', 'NotificationService error createConnection() method:', error);
+    log.log('error', 'Authentication Service error createConnection() method:', error);
     return undefined;
   }
 }
